9931f466df18f0d75d16fb199e6f5f4244519e86
James-ssss <nigmatulin.2003@inbox.ru>
2024-06-13 22:51:03 +0500

    feat: add one semi-working endpoint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from local_settings import postresql as settings
 from db_helper.DBHelper import *
+from fastapi import FastAPI, HTTPException
+from utils.schemas import HotelsResponse, HotelRequest
+import httpx
 
+app = FastAPI()
 keys = ['pguser', 'pgpasswd', 'pghost', 'pgport', 'pgdb']
 if not all(key in keys for key in settings.keys()):
     raise Exception('Bad confid file')
@@ -19,3 +23,23 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/search_hotels", response_model=HotelsResponse)
+async def search_hotels(request: HotelRequest):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(
+                "URL_СЕРВИСА_ОТЕЛЕЙ",
+                json={"city": request.city, "date": request.date}
+            )
+            response.raise_for_status()
+            hotels_data = response.json()
+            return HotelsResponse(hotels=hotels_data["hotels"])
+        except httpx.HTTPStatusError as e:
+            raise   HTTPException(status_code=e.response.status_code, detail=str(e))
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
