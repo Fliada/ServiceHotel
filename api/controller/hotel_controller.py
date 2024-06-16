7f80971be39d07d58ec3f7d107e9c94625f87e84
Ilya Filippov <ilyafilippov@MacBook-Pro-Ilya.local>
2024-06-16 15:04:07 +0500

    feat: :sparkles: реализация API поиска отелей

--- api/controller/hotel_controller.py
+++ api/controller/hotel_controller.py
@@ -1,6 +1,8 @@
-from fastapi import HTTPException, APIRouter
-from utils.schemas import HotelsResponse, HotelRequest
-import httpx
+from datetime import datetime, timedelta
+
+from fastapi import APIRouter, HTTPException
+
+from utils.schemas import HotelRequest, HotelResponse
 
 hotel_routes = APIRouter()
 
@@ -10,18 +12,33 @@ def read_root():
     return {"Hello": "World"}
 
 
-@hotel_routes.post("/search_hotels", response_model=HotelsResponse)
-async def search_hotels(request: HotelRequest):
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(
-                "URL_СЕРВИСА_ОТЕЛЕЙ",
-                json={"city": request.city, "date": request.date}
-            )
-            response.raise_for_status()
-            hotels_data = response.json()
-            return HotelsResponse(hotels=hotels_data["hotels"])
-        except httpx.HTTPStatusError as e:
-            raise HTTPException(status_code=e.response.status_code, detail=str(e))
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+@hotel_routes.post("/search_hotels", response_model=list[HotelResponse])
+async def search_hotels(request: HotelRequest, helper):
+    try:
+        available_hotels = helper.get_hotels_with_available_apartments(
+            start_date=request.date,
+            end_date=(
+                datetime.strptime(request.date, "%Y-%m-%d") + timedelta(days=2)
+            ).strftime("%Y-%m-%d"),
+            city=request.city,
+        )
+
+        filtered_hotels = []
+        for hotel in available_hotels:
+            hotel_info = {
+                "id": hotel["id"],
+                "name": hotel["name"],
+                "location": {
+                    "city": hotel["location"]["city"],
+                    "street": hotel["location"]["street"],
+                    "house_number": hotel["location"]["house_number"],
+                },
+                "min_cost": hotel["min_cost"],
+                "max_cost": hotel["max_cost"],
+            }
+            filtered_hotels.append(HotelResponse(**hotel_info))
+
+        return filtered_hotels
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- utils/schemas.py
+++ utils/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 from datetime import date
+
 class HotelResponse(BaseModel):
     name: str
     location: str
@@ -10,7 +11,13 @@ class HotelResponse(BaseModel):
 
 class HotelsResponse(BaseModel):
     hotels: List[HotelResponse]
+    min_price: Optional[int] = None
+    max_price: Optional[int] = None
 
 class HotelRequest(BaseModel):
     city: str
     date: date
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
+    min_price: Optional[int] = None
+    max_price: Optional[int] = None
