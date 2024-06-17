37397133b48c2171fb3279f8c08a07ec65714468
Tyan v sharfe <thisonlystudy@gmail.com>
2024-06-17 23:35:40 +0500

    feat: 🔥 фиксы

--- api/controller/hotel_controller.py
+++ api/controller/hotel_controller.py
@@ -9,6 +9,7 @@ from config import helper
 
 hotel_routes = APIRouter()
 
+
 @hotel_routes.post("/search_hotels", response_model=list[HotelResponse])
 async def search_hotels(request: HotelRequest):
     try:
@@ -35,26 +36,3 @@ async def search_hotels(request: HotelRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-@hotel_routes.post("/apartments", response_model=list[ApartmentsResponse])
-async def get_apartments_by_ids(request: ApartmentList, helper: DBHelper = Depends()):
-    try:
-        apartments = helper.get_apartments_by_ids(request.apartments)
-
-        result_apartments = []
-        for ap in apartments:
-            apartments_info = {
-                "id": ap.id,
-                "number": ap.number,
-                "hotel_name": ap.hotel.name,
-                "hotel_city": ap.hotel.city,
-                "cost": ap.type.price,
-                "type_capacity": ap.type.capacity,
-                "type_name": ap.type.name
-            }
-            result_apartments.append(ApartmentsResponse(**apartments_info))
-
-        return result_apartments
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
