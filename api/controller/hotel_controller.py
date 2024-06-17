0c388c9aee693a14dbe1ac52f8b5485bb4567867
Tyan v sharfe <thisonlystudy@gmail.com>
2024-06-17 22:05:11 +0500

    feat: :sparkles: добавлен контроллер get_apartments_by_ids

--- api/controller/hotel_controller.py
+++ api/controller/hotel_controller.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timedelta
 from fastapi import APIRouter, HTTPException, Depends
-from utils.schemas import HotelRequest, HotelResponse
-from db_helper.DBHelper import DBHelper 
+
+from dao import hotel
+from utils.schemas import *
+from db_helper.DBHelper import DBHelper
 
 hotel_routes = APIRouter()
+
 
 @hotel_routes.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @hotel_routes.post("/search_hotels", response_model=list[HotelResponse])
 async def search_hotels(request: HotelRequest, helper: DBHelper = Depends()):
@@ -15,7 +19,7 @@ async def search_hotels(request: HotelRequest, helper: DBHelper = Depends()):
         available_hotels = helper.get_hotels_with_available_apartments(
             start_date=request.date,
             end_date=(
-                datetime.strptime(request.date, "%Y-%m-%d") + timedelta(days=2)
+                    datetime.strptime(request.date, "%Y-%m-%d") + timedelta(days=2)
             ).strftime("%Y-%m-%d"),
             city=request.city,
         )
@@ -36,6 +40,30 @@ async def search_hotels(request: HotelRequest, helper: DBHelper = Depends()):
             filtered_hotels.append(HotelResponse(**hotel_info))
 
         return filtered_hotels
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@hotel_routes.post("/apartments", response_model=list[ApartmentsResponse])
+async def get_apartments_by_ids(request: ApartmentList, helper: DBHelper = Depends()):
+    try:
+        apartments = helper.get_apartments_by_ids(request.apartments)
+
+        result_apartments = []
+        for ap in apartments:
+            apartments_info = {
+                "id": ap.id,
+                "number": ap.number,
+                "hotel_name": ap.hotel.name,
+                "hotel_city": ap.hotel.city,
+                "cost": ap.type.price,
+                "type_capacity": ap.type.capacity,
+                "type_name": ap.type.name
+            }
+            result_apartments.append(ApartmentsResponse(**apartments_info))
+
+        return result_apartments
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- utils/schemas.py
+++ utils/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 from datetime import date
 
+
 class HotelResponse(BaseModel):
     name: str
     location: str
@@ -9,10 +10,12 @@ class HotelResponse(BaseModel):
     end_date: date
     price: int
 
+
 class HotelsResponse(BaseModel):
     hotels: List[HotelResponse]
     min_price: Optional[int] = None
     max_price: Optional[int] = None
+
 
 class HotelRequest(BaseModel):
     city: str
@@ -21,3 +24,17 @@ class HotelRequest(BaseModel):
     end_date: Optional[date] = None
     min_price: Optional[int] = None
     max_price: Optional[int] = None
+
+
+class ApartmentsResponse(BaseModel):
+    id: int
+    number: int
+    hotel_name: str
+    hotel_city: str
+    cost: int
+    type_capacity: int
+    type_name: str
+
+
+class ApartmentList(BaseModel):
+    apartments: list[int] = []
